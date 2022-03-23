e245ea71d295196b7e48e6d8f64161e78ed952c9
Sanjeet Mistry <sanjeetm@axioned.com>
2022-03-23 18:40:48 +0530

    Change '_redirects' file name

--- .eleventy.js
+++ .eleventy.js
@@ -9,21 +9,21 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.setBrowserSyncConfig({
         callbacks: {
-          ready: function(err, bs) {
-    
-            bs.addMiddleware("*", (req, res) => {
-              if (!fs.existsSync(NOT_FOUND_PATH)) {
-                throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
-              }
-    
-              const content_404 = fs.readFileSync(NOT_FOUND_PATH);
-              // Add 404 http status code in request header.
-              res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
-              // Provides the 404 content without redirect.
-              res.write(content_404);
-              res.end();
-            });
-          }
+			ready: function(err, bs) {
+		
+				bs.addMiddleware("*", (req, res) => {
+				if (!fs.existsSync(NOT_FOUND_PATH)) {
+					throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
+				}
+		
+				const content_404 = fs.readFileSync(NOT_FOUND_PATH);
+				// Add 404 http status code in request header.
+				res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+				// Provides the 404 content without redirect.
+				res.write(content_404);
+				res.end();
+				});
+			}
         }
-      });
+	});
 };
